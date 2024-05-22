fb5635991614827ef967cab2809142e7d6e76d8d
Chathura <cpriydasun@gmail.com>
2024-05-23 01:35:45 +0530

    update

---
+++ samplePipeline4_pipelineAsCodeInSingleShell.groovy
@@ -0,0 +1,16 @@
+pipeline {
+    agent any
+    stages {
+        stage ('one') {
+
+            // Checkout the code from the repository
+            git branch: 'main' url: 'https://github.com/PriyadasunKC/jenkins.git'
+            // build test get artifcats using steps in a single shell
+            sh '''
+                mvn compile
+                mvn test
+                mvn clean package
+            '''
+        }
+    }
+}
